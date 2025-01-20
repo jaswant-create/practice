e2b62fc15e8100c6e0ae3bcd17ec69425506bb63
jaswant-create <jaswant@ultimatelyfstyle.com>
2025-01-20 06:06:29 +0000

    jsmater

---
+++ jsmaster.js
@@ -0,0 +1,19 @@
+// let y = "5";
+// console.log(typeof y);  // Output: "string"
+
+// let x = +"t";  // The unary plus converts 'y' from string to number
+// console.log(typeof x);  // Output: "number"
+
+// let x = 123;
+// console.log(typeof String(x));
+// console.log(typeof String(123));
+// // console.log(typeof String(100 + 23));
+
+console.log((5 + null) + "\n" + 
+            ("5" + null) + "\n" + 
+            ("5" + 2) + "\n" + 
+            ("5" - 2) + "\n" + 
+            ("5" * "2") + "\n" + 
+            ("5" / "2"));
+
+
